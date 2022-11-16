a9a62951692e4b40703e0c03b6184902cfbb4365
Admin <Admin@TEB123001>
2022-11-16 12:14:33 +0100

    Add project files.

---
+++ Blackjack Projekt/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace World_Of_Warcraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random slump = new Random();
+            
+
+            Console.WriteLine("Select your class.");
+            Console.WriteLine(".1 Mage");
+            Console.WriteLine(".2 Warrior");
+            Console.WriteLine(".3 Warlock");
+            Console.WriteLine(".4 Rouge");
+            Console.WriteLine(".5 Hunter");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+
+                    Console.WriteLine("Du valde mage " + slump.Next(6000, 9001));
+                    if (slump.Next(6000, 9000) > 7000)
+                    {
+                        Console.WriteLine("Din mage har big gear med good pump");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Din Mage har inte bra gear med shit pump");
+                    }
+
+                    break;
+
+            }
+
+
+        }
+    }
+    
+}
